1fcdce3cb77146ed60f2e9ac185ab277e033a742
Jonathan Ostrander <jonathan@bam-x.com>
2016-10-05 19:04:14 -0400

    drawing

--- js/index.js
+++ js/index.js
@@ -1,32 +1,37 @@
 function toHex(n) {
-  const hex = n.toString();
-  return hex.length === 2 ? hex : `0{hex}`;
+  const hex = parseInt(n).toString(16);
+  return hex.length === 2 ? hex : `0${hex}`;
 }
 
-function hsvToHex(h, s, v) {
+function hueToRgb(p, q, t) {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1/6) return p + (q - p) * 6 * t;
+  if (t < 1/2) return q;
+  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  return p;
+}
+
+function hslToHex(h, s, l){
     let r, g, b;
 
-    const i = Math.floor(h * 6);
-    const f = h * 6 - i;
-    const p = v * (1 - s);
-    const q = v * (1 - f * s);
-    const t = v * (1 - (1 - f) * s);
-
-    switch(i % 6){
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        let p = 2 * l - q;
+        r = hueToRgb(p, q, h + 1/3);
+        g = hueToRgb(p, q, h);
+        b = hueToRgb(p, q, h - 1/3);
     }
 
-    return `#{toHex(255*r)}{toHex(255*g)}{toHex(255*b)}`;
-};
+    return `#${toHex(255*r)}${toHex(255*g)}${toHex(255*b)}`;
+}
 
 class CanvasCursor {
-  constructor(canvas, color='#FFFFFF', size=20) {
+  constructor(canvas, display, color='#FFFFFF', size=20) {
     this.canvas = canvas;
+    this.display = display;
     this.color = color;
     this.size = size;
     this.coors = [];
@@ -52,14 +57,13 @@ class CanvasCursor {
 
   _getCoors(event) {
     const rect = this.canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left + document.body.scrollLeft + document.documentElement.scrollLeft;
-    const y = event.clientY - rect.top + document.body.scrollTop + document.documentElement.scrollTop;
+    const x = event.clientX - rect.left + document.documentElement.scrollLeft;
+    const y = event.clientY - rect.top + document.documentElement.scrollTop;
     return [x, y];
   }
 
   _draw(x, y) {
     const context = this.canvas.getContext("2d");
-    console.log(context);
     context.strokeStyle = this.color;
     context.lineWidth = this.size;
     context.lineCap = "round";
@@ -69,18 +73,38 @@ class CanvasCursor {
     context.stroke();
     this.coors = [x, y];
   }
+
+  updateDisplay() {
+    this.display.style.backgroundColor = this.color;
+    this.display.style.height = `${this.size}px`;
+    this.display.style.width = `${this.size}px`;
+  }
 };
 
 class ColorPicker {
-  constructor(el, color) {
-    this.el = el;
-    this.slider = el.querySelector('.hue');
-    this.sliderPicker = this.slider.querySelector('.slider-picker');
-    this.pickerArea = el.querySelector('.picking-area');
-    this.picker = this.pickerArea.querySelector('.picker');
-    this.color = color;
+  constructor(areaEl, sliderEl, cursor) {
+    this.pickerX = 100;
+    this.pickerY = 100;
+    this.sliderY = 100;
+    this.cursor = cursor;
+    this.slider = sliderEl;
+    this.sliderPicker = this.slider.querySelector('#hue-slider');
+    this.pickerArea = areaEl;
+    this.picker = this.pickerArea.querySelector('#color-area-picker');
     this._addListeners();
-    // this._updateSliders();
+    this._updateSlidersAndColor();
+  }
+
+  _updateSlidersAndColor() {
+    this.sliderPicker.style.top = `${this.sliderY}px`;
+
+    this.picker.style.left = `${this.pickerX}px`;
+    this.picker.style.top = `${this.pickerY}px`;
+
+    this.pickerArea.style.backgroundColor = hslToHex(this.sliderY/198, 1, 0.5);
+
+    this.cursor.color = hslToHex(this.sliderY/198, (this.pickerX + 6)/200, 1 - (this.pickerY + 6)/200);
+    this.cursor.updateDisplay();
   }
 
   _addListeners() {
@@ -91,13 +115,13 @@ class ColorPicker {
   _addSliderListener() {
     const mousemove = (event) => {
       const rect = this.slider.getBoundingClientRect();
-      const y = Math.max(Math.min(event.clientY - rect.top - 3, 198), 0);
-      this.sliderPicker.style.top = `${y}px`;
+      this.sliderY = Math.max(Math.min(event.clientY - rect.top - 3, 198), 0);
+      this._updateSlidersAndColor();
     };
     this.slider.addEventListener('mousedown', (event) => {
       const rect = this.slider.getBoundingClientRect();
-      const y = event.clientY - rect.top - 3;
-      this.sliderPicker.style.top = `${y}px`;
+      this.sliderY = Math.max(Math.min(event.clientY - rect.top - 3, 198), 0);
+      this._updateSlidersAndColor();
       this.slider.addEventListener('mousemove', mousemove, false);
       window.addEventListener('mouseup', () => {
         this.slider.removeEventListener('mousemove', mousemove, false);
@@ -108,17 +132,15 @@ class ColorPicker {
   _addPickerListener() {
     const mousemove = (event) => {
       const rect = this.pickerArea.getBoundingClientRect();
-      const x = Math.max(Math.min(event.clientX - rect.left - 8, 194), -6);
-      const y = Math.max(Math.min(event.clientY - rect.top - 6, 194), -6);
-      this.picker.style.left = `${x}px`;
-      this.picker.style.top = `${y}px`;
+      this.pickerX = Math.max(Math.min(event.clientX - rect.left - 8, 194), -6);
+      this.pickerY = Math.max(Math.min(event.clientY - rect.top - 6, 194), -6);
+      this._updateSlidersAndColor();
     };
     this.pickerArea.addEventListener('mousedown', (event) => {
       const rect = this.pickerArea.getBoundingClientRect();
-      const x = Math.max(Math.min(event.clientX - rect.left - 8, 194), -6);
-      const y = Math.max(Math.min(event.clientY - rect.top - 6, 194), -6);
-      this.picker.style.left = `${x}px`;
-      this.picker.style.top = `${y}px`;
+      this.pickerX = Math.max(Math.min(event.clientX - rect.left - 8, 194), -6);
+      this.pickerY = Math.max(Math.min(event.clientY - rect.top - 6, 194), -6);
+      this._updateSlidersAndColor();
       this.pickerArea.addEventListener('mousemove', mousemove, false);
       window.addEventListener('mouseup', () => {
         this.pickerArea.removeEventListener('mousemove', mousemove, false);
@@ -127,15 +149,35 @@ class ColorPicker {
   }
 }
 
+class SizePicker {
+  constructor(el, cursor) {
+    this.el = el;
+    this.cursor = cursor;
+    this.el.value = this.cursor.size;
+    this._addInputListener();
+  }
+
+  _addInputListener() {
+    this.el.addEventListener('input', () => {
+      this.cursor.size = this.el.value;
+      this.cursor.updateDisplay();
+    });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const canvasDiv = document.getElementById('canvas-div');
   const canvas = document.createElement('canvas');
-  canvas.height = 300;
-  canvas.width = 300;
+  canvas.height = 500;
+  canvas.width = 1000;
   canvasDiv.appendChild(canvas);
   const color = '#000000';
-  new CanvasCursor(canvas, color, 20);
+  const displayDiv = document.querySelector('#cursor-display > div');
+  const cursor = new CanvasCursor(canvas, displayDiv, color, 20);
 
-  const pickerEl = document.getElementById('color-picker');
-  new ColorPicker(pickerEl, '#000000');
+  const areaEl = document.getElementById('color-area');
+  const sliderEl = document.getElementById('hue-picker');
+  new ColorPicker(areaEl, sliderEl, cursor);
+  const sizeSlider = document.querySelector('#size-picker input');
+  new SizePicker(sizeSlider, cursor);
 });
